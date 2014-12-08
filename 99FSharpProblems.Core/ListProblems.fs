34a0dbdac234e2210432ef974604d54346b6cadc
Paul Marfleet <paul.marfleet@gmail.com>
2014-12-08 21:39:57 +0000

    Created Core and Test projects Added impl + tests for Problem 1

---
+++ 99FSharpProblems.Core/ListProblems.fs
@@ -0,0 +1,11 @@
+﻿namespace _99FSharpProblems.Core
+
+module ListProblems =
+
+    let Problem1 input =
+        let rec getlast remaining =
+            match remaining with
+            | hd :: tl when tl.IsEmpty -> hd
+            | _ :: tl -> getlast tl
+            | [] -> failwith "Empty list"
+        getlast input
---
+++ 99FSharpProblems.Tests/ListProblemsTests.fs
@@ -0,0 +1,18 @@
+﻿namespace _99FSharpProblems.Tests
+
+open Xunit
+open Xunit.Extensions
+open FsUnit.Xunit
+open _99FSharpProblems.Core
+
+module ListProblemsTests =
+
+    module Problem1Tests =
+
+        [<Fact>]
+        let ``Last element of list containing elements 1,2,3,4 should be 4``() =
+            ListProblems.Problem1 [1..4] |> should equal 4
+
+        [<Fact>]
+        let ``Last element of list containing elements x,y,z should be z``() =
+            ListProblems.Problem1 ['x'..'z'] |> should equal 'z'
