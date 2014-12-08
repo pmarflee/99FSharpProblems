b151ed20ca56c9425d3ce0ec8c1300654128a19f
Paul Marfleet <paul.marfleet@gmail.com>
2014-12-08 21:46:05 +0000

    Simplified implementation of Problem 1 Added impl + tests for Problem 2

--- 99FSharpProblems.Core/ListProblems.fs
+++ 99FSharpProblems.Core/ListProblems.fs
@@ -5,7 +5,15 @@ module ListProblems =
     let Problem1 input =
         let rec getlast remaining =
             match remaining with
-            | hd :: tl when tl.IsEmpty -> hd
+            | hd :: [] -> hd
             | _ :: tl -> getlast tl
             | [] -> failwith "Empty list"
         getlast input
+
+    let Problem2 input =
+        let rec getlastbut1 remaining =
+            match remaining with
+            | hd :: last :: [] -> hd
+            | _ :: tl -> getlastbut1 tl
+            | [] -> failwith "Empty list"
+        getlastbut1 input
--- 99FSharpProblems.Tests/ListProblemsTests.fs
+++ 99FSharpProblems.Tests/ListProblemsTests.fs
@@ -16,3 +16,13 @@ module ListProblemsTests =
         [<Fact>]
         let ``Last element of list containing elements x,y,z should be z``() =
             ListProblems.Problem1 ['x'..'z'] |> should equal 'z'
+
+    module Problem2Tests =
+
+        [<Fact>]
+        let ``Last but 1 element of list containing elements 1,2,3,4 should be 3``() =
+            ListProblems.Problem2 [1..4] |> should equal 3
+
+        [<Fact>]
+        let ``Last but 1 element of list containing elements x,y,z should be y``() =
+            ListProblems.Problem2 ['x'..'z'] |> should equal 'y'
